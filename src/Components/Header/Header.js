2c6b4beab27182a913ceea27a61c72999bd18c01
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-15 15:31:13 +0600

    azure release pipeline added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <>
-            <AppBar position="static" className="bg-dark">
+            <AppBar position="sticky" className="bg-dark">
                 <Toolbar>
                     <Image className="p-2 ml-3" height={50} src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Yammer_logo.svg/1024px-Yammer_logo.svg.png"/>
                     <Link to="/home">
